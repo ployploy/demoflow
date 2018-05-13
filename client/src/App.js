da61498849296a15f52283325b893fa3f9119b4b
ploy <chonnikan.t@kkumail.com>
2018-05-13 13:48:46 +0700

    [Add] สร้างตัวไปเรียก api และนำมาแสดงผลหน้าเว็ป

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3001/health")
+    .then(result => result.text())
+    .then(data => {
+       this.setState({ health: data });
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Server status: {this.state.health}</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
