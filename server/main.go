272aa59a4cdc8c365f3b7db760ba316f4a6fb019
ploy <chonnikan.t@kkumail.com>
2018-05-13 14:19:18 +0700

    [Add] เพิ่ม handler user เข้ามา

--- server/main.go
+++ server/main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
+type User struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
 func main() {
 	r := mux.NewRouter()
 
 	//1 routing per 1 handler
 	r.Handle("/health", HealthHandler).Methods("GET")
-	r.Handle("/users", GetUserHandler).Methods("GET")
+	r.Handle("/user", GetUserHandler).Methods("GET")
 
 	//วิธีกำหนดคนเข้าถึงกรณีเรียกใช้งานข้าม IP
 	corsObj := handlers.AllowedOrigins([]string{"*"})
@@ -27,6 +33,7 @@ var HealthHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request
 })
 
 var GetUserHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	//เรียกใช้ service อื่นที่ต้องใช้งาน
-	w.Write([]byte("chonnikan"))
+	user := User{Username: "ploy", Email: "chonnikan@gmail.com"}
+	response, _ := json.Marshal(user)
+	w.Write([]byte(response))
 })
